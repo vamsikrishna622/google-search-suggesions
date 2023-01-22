5b78bac66ec68a05a3660921716349219e21bc3f
vamsikrishna622 <vamsikrishnarapuru2005@gmail.com>
2023-01-22 17:36:12 +0000

    googleSearchSuggestions

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,28 @@
+// Write your code here
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestionDetails, updateSearchInput} = props
+  const {suggestion} = suggestionDetails
+
+  const onClickSuggestion = () => {
+    updateSearchInput(suggestion)
+  }
+  return (
+    <li className="list-item-container">
+      <p className="suggestion-text">{suggestion}</p>
+      <button
+        type="button"
+        className="arrow-button"
+        onClick={onClickSuggestion}
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          className="arrow"
+          alt="arrow"
+        />
+      </button>
+    </li>
+  )
+}
+export default SuggestionItem
